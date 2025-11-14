8db0e8b8810d39125d2e33a5e334c15b1fea8776
Arthur-FPM <An4rk.social@gmail.com>
2025-11-13 21:18:50 -0300

    Add files via upload

---
+++ objeto.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome: "Arthur",
+    idade:24,
+    peso:75,
+    altura:1.74
+}
+//meu nome é --- tenho --- de idade, peso---kg e minha altura é---m 
+console.log(`Meu nome é ${pessoa.nome} tenho ${pessoa.idade} anos, peso ${pessoa.peso}kg e minha altura é ${pessoa.altura}`)
